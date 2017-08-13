e771f5cd281654f6983ec315715462e2df2b5ca6
Alvin Stockhaus <alvin@stockhaus.ninja>
2017-08-13 17:51:46 +0200

    Initial sketch of game architecture

--- src/index.js
+++ src/index.js
@@ -1,8 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import './index.css'
+
+const init = () => {
+    var root = document.getElementById('root')
+    var canvas = document.createElement('canvas')
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+    root.appendChild(canvas)
+    return canvas.getContext("2d")
+}
+
+const rootReducer = (state = { hello: "world" }, action) => {
+    switch(action.type) {
+        case "HELLO_WORLD":
+            return Object.assign({}, state, { hello: "Hello World!" })
+
+        default:
+            return state
+    }
+}
+
+const helloWorld = () => ({
+    type: "HELLO_WORLD"
+})
+
+var currentValue
+const handleChange = (getState) => () => {
+    const previousValue = currentValue
+    currentValue = getState()
+
+    if(currentValue !== previousValue) {
+        console.log("I handled a change, yo!")
+    }
+}
+
+var context = init()
+
+var store = createStore(rootReducer)
+
+var unsubscribe = store.subscribe(handleChange(store.getState, context))
+
+store.dispatch(helloWorld())
