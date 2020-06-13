fe390b6935ce46a105ca7b3d9f6b94c1d0d9ed84
Alvin Stockhaus <alvins@opera.com>
2020-06-13 11:11:04 +0200

    Updates

---
+++ precache-manifest.b6ac3a771188425af51475aad6712d17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15407b8e8889fe47f212ed7c3312b4dd",
+    "url": "/pixflaw_redux/index.html"
+  },
+  {
+    "revision": "277317451ae2e177e854",
+    "url": "/pixflaw_redux/static/css/main.ff01f609.chunk.css"
+  },
+  {
+    "revision": "5197ce2c6807638e678c",
+    "url": "/pixflaw_redux/static/js/2.09238a1a.chunk.js"
+  },
+  {
+    "revision": "277317451ae2e177e854",
+    "url": "/pixflaw_redux/static/js/main.7140d3d0.chunk.js"
+  },
+  {
+    "revision": "eb94f5a6dd385a08f2ad",
+    "url": "/pixflaw_redux/static/js/runtime-main.3943dd47.js"
+  }
+]);
